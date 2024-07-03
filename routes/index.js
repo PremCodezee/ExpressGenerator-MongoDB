b911e18cfd71d7928a6fa0eab59eab438eb9dce8
Prem Codezee <prem.codezee.gmail.com>
2024-07-03 18:29:48 +0530

    Done Cookie and Session

--- routes/index.js
+++ routes/index.js
@@ -2,28 +2,71 @@ var express = require("express");
 var router = express.Router();
 const userModel = require("./users");
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
 
-router.get("/create", async function (req, res) {
-  const createdUser = await userModel.create({
-    name: "john cena",
-    email: "johncena@gmail.com",
-    age: 10,
-  });
+// // cookie
 
-  res.send(createdUser);
-});
+// router.get("/", function (req, res, next) {
+//   res.cookie("username", "john cena", { maxAge: 900000, httpOnly: false });
+//   console.log(req.cookies);
+//   res.render(`index`, { title: "Express" });
+// });
 
-router.get("/read", async function (req, res) {
-  const users = await userModel.find();
-  // find is to get all users
-  // instead find you can use findOne to find only one data
-  // const user = await userModel.findOne({ name: "john cena" });
-  res.send(users);
-});
+// router.get("/checkCookie", function (req, res) {
+//   console.log(req.cookies);
+//   res.send(req.cookies);
+// });
+
+// router.get("/deleteCookie", function (req, res) {
+//   res.clearCookie("username");
+//   console.log(req.cookies);
+//   res.redirect("/checkCookie");
+// })
+
+
+// session
+// router.get("/", function (req, res, next) {
+//   req.session.banned = true;
+//   console.log(req.session);
+//   res.render("index", { title: "Express" });
+// });
+
+// router.get("/checkBan", function(req, res) {
+//   if (req.session.banned === true) {
+//     res.send("you are banned");
+//   } else {
+//     res.send("you are not banned");
+//   }
+// })
+
+// router.get("/removeBan", function(req, res) {
+//   req.session.destroy((err) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     // check ban
+//     res.redirect("/checkBan");
+//   })
+// })
+
+
+// mongo
+// router.get("/create", async function (req, res) {
+//   const createdUser = await userModel.create({
+//     name: "john cena",
+//     email: "johncena@gmail.com",
+//     age: 10,
+//   });
+
+//   res.send(createdUser);
+// });
+
+// router.get("/read", async function (req, res) {
+//   const users = await userModel.find();
+//   // find is to get all users
+//   // instead find you can use findOne to find only one data
+//   // const user = await userModel.findOne({ name: "john cena" });
+//   res.send(users);
+// });
 
 // router.get("/update", async function (req, res) {
 //   const updatedUser = await userModel.findOneAndUpdate(
@@ -34,10 +77,10 @@ router.get("/read", async function (req, res) {
 //   res.send(updatedUser);
 // });
 
-router.get("/delete", async function (req, res) {
-  const deletedUser = await userModel.findOneAndDelete({ name: "test" });
+// router.get("/delete", async function (req, res) {
+//   const deletedUser = await userModel.findOneAndDelete({ name: "test" });
 
-  res.send(deletedUser);
-});
+//   res.send(deletedUser);
+// });
 
 module.exports = router;
