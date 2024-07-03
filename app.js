de9d79db75311c39f5ab3509e2078ff7bcd85061
Prem Codezee <prem.codezee.gmail.com>
2024-07-03 16:39:33 +0530

    CRUD using express in mongo

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+
 
 const app = express();
 
@@ -20,7 +20,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,7 +38,7 @@ app.use(function(err, req, res, next) {
 });
 
 app.listen(3001, () => {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3001!');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const userModel = require("./users");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/create", async function (req, res) {
+  const createdUser = await userModel.create({
+    name: "john cena",
+    email: "johncena@gmail.com",
+    age: 10,
+  });
+
+  res.send(createdUser);
+});
+
+router.get("/read", async function (req, res) {
+  const users = await userModel.find();
+  // find is to get all users
+  // instead find you can use findOne to find only one data
+  // const user = await userModel.findOne({ name: "john cena" });
+  res.send(users);
+});
+
+// router.get("/update", async function (req, res) {
+//   const updatedUser = await userModel.findOneAndUpdate(
+//     { name: "john cena" },
+//     { name: "john cena updated" }
+//   );
+
+//   res.send(updatedUser);
+// });
+
+router.get("/delete", async function (req, res) {
+  const deletedUser = await userModel.findOneAndDelete({ name: "test" });
+
+  res.send(deletedUser);
 });
 
 module.exports = router;
